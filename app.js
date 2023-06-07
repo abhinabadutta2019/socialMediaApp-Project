266482493b4c9e4bd35b13c8fef4355d60cd15a4
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-06-07 16:58:38 +0530

    image uploaded to local file, not showing by mongodbid, in browser

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
+//
+const Image = require("./models/imageModel");
 
 //
 const userRoutes = require("./routes/userRoutes");
@@ -15,8 +17,10 @@ const storage = multer.diskStorage({
     cb(null, "images");
   },
   filename: (req, file, cb) => {
-    console.log(file);
-    cb(null, Date.now() + path.extname(file.originalname));
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const fileExtension = path.extname(file.originalname);
+    const filename = req.params.id + "-" + uniqueSuffix + fileExtension;
+    cb(null, filename);
   },
 });
 const upload = multer({ storage: storage });
@@ -53,11 +57,37 @@ app.get("/", (req, res) => {
 app.use("/images", express.static("images"));
 //
 app.get("/upload", (req, res) => {
-  res.render("upload");
+  try {
+    res.render("upload");
+  } catch (err) {
+    res.json(err);
+  }
 });
 //
-app.post("/upload", upload.single("image"), (req, res) => {
-  res.send("image uploaded");
+app.post("/upload", upload.single("image"), async (req, res) => {
+  try {
+    const newImage = new Image({
+      image: {
+        data: req.file.filename, // Save the file name
+        contentType: req.file.mimetype, // Use the actual file's content type
+      },
+    });
+    const image = await newImage.save();
+    res.send({ image: image });
+  } catch (err) {
+    res.json(err);
+  }
+});
+//
+app.get("/showImage/:id", async (req, res) => {
+  try {
+    const oneImage = await Image.findById(req.params.id);
+    const imageBase64 = oneImage.image.data.toString("base64");
+    const imageSrc = `data:${oneImage.image.contentType};base64,${imageBase64}`;
+    res.render("showImage", { imageSrc });
+  } catch (err) {
+    res.json(err);
+  }
 });
 //
 app.use("/user", userRoutes);
